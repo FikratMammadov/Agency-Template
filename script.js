44124868b2d73a903d5e247bfab82fdaee27ff4f
Fikrat Mammadov <fikrat.mammadov.a@asoiu.edu.az>
2021-02-10 14:00:05 +0400

    Agency Template

---
+++ script.js
@@ -0,0 +1,10 @@
+var services=document.getElementById('services');
+var portfolio=document.getElementById('portfolio');
+var about=document.getElementById('about');
+var team=document.getElementById('team');
+var contact=document.getElementById('contact');
+
+
+window.addEventListener('scroll',function(e){
+    console.log(e.target.scrollY);
+})
